30678a9233b9c8be0bb45722306a9346405b8c45
Jake Scott <getsuperlogical@gmail.com>
2011-01-21 18:38:27 +0000

    adding a mvc3 example

---
+++ ServiceStackAppHarborMVC3/AppHost.cs
@@ -0,0 +1,45 @@
+namespace ServiceStackAppHarborMVC3
+{
+    using System;
+    using Funq;
+    using ServiceStack.CacheAccess;
+    using ServiceStack.CacheAccess.Providers;
+    using ServiceStack.Configuration;
+    using ServiceStack.Logging;
+    using ServiceStack.WebHost.Endpoints;
+    using ServiceStackAppHarbor.ServiceModel;
+
+    public class AppHost : AppHostBase
+    {
+        private static ILog log;
+
+        public AppHost()
+            : base("ServiceStackAppHarbor AspNet", typeof(GetFactorialService).Assembly)
+        {
+            LogManager.LogFactory = new ServiceStack.Logging.Support.Logging.ConsoleLogFactory();
+            log = LogManager.GetLogger(typeof(AppHost));
+        }
+
+        public override void Configure(Container container)
+        {
+            // Signal advanced web browsers what HTTP Methods you accept
+            base.SetConfig(new EndpointHostConfig()
+            {
+                GlobalResponseHeaders = 
+                {
+                    { "Access-Control-Allow-Origin", "*" },
+					{ "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" },
+                }
+            });
+
+            container.Register<IResourceManager>(new ConfigurationResourceManager());
+
+            container.Register(c => new ExampleConfig(c.Resolve<IResourceManager>()));
+            var appConfig = container.Resolve<ExampleConfig>();
+
+            container.Register<ICacheClient>(c => new MemoryCacheClient());
+
+            log.InfoFormat("AppHost Configured: " + DateTime.Now);
+        }
+    }
+}
---
+++ ServiceStackAppHarborMVC3/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ServiceStackAppHarborMVC3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.IgnoreRoute("servicestack/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "App", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            var appHost = new AppHost();
+            appHost.Init();
+        }
+    }
+}
